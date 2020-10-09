352c6ce043bac0d328258b2db7f52040359a8bb3
Mohana Kavya K <kkunisettymohana@student.nitw.ac.in>
2020-10-09 12:43:40 +0530

    Modified : Printout of Maximum using Generic Method

--- main/java/com/capgemini/assignment/findmaximum/FindMaximum.java
+++ main/java/com/capgemini/assignment/findmaximum/FindMaximum.java
@@ -5,25 +5,28 @@ import java.util.Scanner;
 
 public class FindMaximum <E extends Comparable<E>> {
 	public static Scanner sc=new Scanner(System.in);
-	
 	E max;
-	
 	public FindMaximum() {
-	
+		
 	}
 	
 	public E printMax(){
-	    return this.max;	
+		return this.max;
 	}
-
+	
 	public E findMaximum(E ...values) {
 		Arrays.sort(values);
 		this.max=values[values.length-1];
 		return max;
 	}
 
+	public void toPrintMaximum() {
+		System.out.println(max);
+	}
+	
 	public static void main(String[] args) {
-		System.out.println("Welcome to Test Maximum");
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Test maximum Code");
 	}
 
 }
