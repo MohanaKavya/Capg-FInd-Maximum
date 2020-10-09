2961e7d570500e4083642caeae30a5e150ab7e2d
Mohana Kavya K <kkunisettymohana@student.nitw.ac.in>
2020-10-09 11:35:26 +0530

    Refactored : Refactored to Single Generic Method to Find Maximum

--- main/java/com/capgemini/assignment/findmaximum/FindMaximum.java
+++ main/java/com/capgemini/assignment/findmaximum/FindMaximum.java
@@ -5,25 +5,38 @@ import java.util.Scanner;
 public class FindMaximum {
 	public static Scanner sc=new Scanner(System.in);
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to Test Maximum");
-		 System.out.println("Enter three Strings");
-	        String num1 = sc.next();
-	        String num2 = sc.next();
-	        String num3 = sc.next();
-	        System.out.println("Maximum number is "+findMaximum(num1 , num2 , num3));
-	}
-
-	static String findMaximum(String x, String y, String z) {
-		String max=x;
-		if(x.compareTo(y)>0 && x.compareTo(z)>0) {
-			max=x;
-		}else if(z.compareTo(y)>0 && z.compareTo(x)>0) {
-			max=z;
+	static <E extends Comparable<E>> E findMaximum(E x, E y, E z) {
+		E max = x;
+		if (x.compareTo(y) > 0 && x.compareTo(z) > 0) {
+			max = x;
+		} else if (z.compareTo(y) > 0 && z.compareTo(x) > 0) {
+			max = z;
 		} else {
-			max=y;
+			max = y;
 		}
 		return max;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Test Maximum");
+
+		System.out.println("Enter hree integer variables");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		System.out.println("Maximum number is " + findMaximum(a, b, c));
+
+		System.out.println("Enter three float variables");
+		float x = sc.nextFloat();
+		float y = sc.nextFloat();
+		float z = sc.nextFloat();
+		System.out.println("Maximum number is " + findMaximum(x, y, z));
+
+		System.out.println("Enter three Strings");
+		String str1 = sc.next();
+		String str2 = sc.next();
+		String str3 = sc.next();
+		System.out.println("Maximum string is " + findMaximum(str1, str2, str3));
 	}
 
 }
--- test/java/com/capgemini/assignment/findmaximum/TestMaximum.java
+++ test/java/com/capgemini/assignment/findmaximum/TestMaximum.java
@@ -6,6 +6,72 @@ import org.junit.Test;
 public class TestMaximum {
 	
 	@Test
+	public void testIntegerMaximumAtFirstPosition() {
+		Integer maximum=FindMaximum.findMaximum(23, 12, 15);
+		boolean flag;
+		if(maximum.equals(23))
+			flag=true;
+		else
+			flag=false;
+		Assert.assertEquals(true, flag);
+	}
+	
+	@Test
+	public void testIntegerMaximumAtSecondPosition() {
+		Integer maximum=FindMaximum.findMaximum(10, 20, 15);
+		boolean flag;
+		if(maximum.equals(20))
+			flag=true;
+		else
+			flag=false;
+		Assert.assertEquals(true, flag);
+	}
+	
+	@Test
+	public void testIntegerMaximumAtThirdPosition() {
+		Integer maximum=FindMaximum.findMaximum(23, 12, 40);
+		boolean flag;
+		if(maximum.equals(40))
+			flag=true;
+		else
+			flag=false;
+		Assert.assertEquals(true, flag);
+	}
+	
+	@Test
+	public void testFloatMaximumAtFirstPosition() {
+		Float maximum=FindMaximum.findMaximum(23.2f, 12.4f, 15.6f);
+		boolean flag;
+		if(maximum.equals(23.2f))
+			flag=true;
+		else
+			flag=false;
+		Assert.assertEquals(true, flag);
+	}
+	
+	@Test
+	public void testFloatMaximumAtSecondPosition() {
+		Float maximum=FindMaximum.findMaximum(12.5f,29.6f,9.8f);
+		boolean flag;
+		if(maximum.equals(29.6f))
+			flag=true;
+		else
+			flag=false;
+		Assert.assertEquals(true, flag);
+	}
+	
+	@Test
+	public void testFloatMaximumAtThirdPosition() {
+		Float maximum=FindMaximum.findMaximum(14.5f,9.2f,15.1f);
+		boolean flag;
+		if(maximum.equals(15.1f))
+			flag=true;
+		else
+			flag=false;
+		Assert.assertEquals(true, flag);
+	}
+		
+	@Test
 	public void testMaximumAtFirstPosition() {
 		String maximum = FindMaximum.findMaximum("Peach", "Apple", "Banana");
 		boolean flag;
